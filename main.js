d57c4b79fc02d36536573b3c83400247b7977ca3
avalynn24 <avalynn.annarelli@innovationcharter.org>
2025-09-11 18:39:57 +0000

    its mad and we dont know why

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Interface");
-let name = await ti.prompt("What is your name?");
-ti.output("Hello, " + name);
-
+let ti = new TextInterface(app, "Linux");
+ti.output("Welcome to the Linux distro. Guess your program!");
+let above10 = (ti.promptYesOrNo("Does your distro start with K and D?"));
+if (above10===1)
+  let greaterThan5 =
+}
