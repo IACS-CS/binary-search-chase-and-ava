d6e15700b5821134f634fb759808260a2f418c20
probablynotgrady <ipodidiot1000@gmail.com>
2025-09-15 12:25:57 +0000

    changed name of some variables

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ let app = document.querySelector("#app");
 let ti = new TextInterface(app, "Linux Distro Guessing Game");
 ti.output("Welcome to the Linux distro Guessing Program!");
 ti.output("This program has a list of the 20 most well-known Linux distros.");
-let above10 = await (ti.promptYesOrNo("Does your distro start with the letters K and D?"));
-if (above10===true){
+let greaterThan10 = await (ti.promptYesOrNo("Does your distro start with the letters K and D?"));
+if (greaterThan10===true){
   let greaterThan5 = await (ti.promptYesOrNo("THIS IS A PLACEHOLDER TO MAKE THE CODE COMMIT PROPERLY"));
 } else {
   ti.output("THIS IS YET ANOTHER PIECE OF PLACEHOLDER TEXT. SORRY!");
