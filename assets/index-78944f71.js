36234f65cc9621b796bf5fb27cae19f089a3ea72
probablynotgrady <probablynotgrady@users.noreply.github.com>
2025-09-15 12:26:14 +0000

    deploy: d6e15700b5821134f634fb759808260a2f418c20

--- assets/index-78944f71.js
+++ assets/index-78944f71.js
@@ -200,8 +200,8 @@ let app = document.querySelector("#app");
 let ti = new a(app, "Linux Distro Guessing Game");
 ti.output("Welcome to the Linux distro Guessing Program!");
 ti.output("This program has a list of the 20 most well-known Linux distros.");
-let above10 = await (ti.promptYesOrNo("Does your distro start with the letters K and D?"));
-if (above10===true){
+let greaterThan10 = await (ti.promptYesOrNo("Does your distro start with the letters K and D?"));
+if (greaterThan10===true){
   await (ti.promptYesOrNo("THIS IS A PLACEHOLDER TO MAKE THE CODE COMMIT PROPERLY"));
 } else {
   ti.output("THIS IS YET ANOTHER PIECE OF PLACEHOLDER TEXT. SORRY!");
