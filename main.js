512ad4a6029632f7fa0368a45e14342bb4a17fbf
avalynn24 <avalynn.annarelli@innovationcharter.org>
2025-09-17 16:27:50 +0000

    v0.0.3, added more options and added a broken fedora option

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let app = document.querySelector("#app");
 let ti = new TextInterface(app, "Linux Distro Guessing Game");
 let greaterThanD;
 let isDebian;
+let no = false;
 ti.output("Welcome to the Linux distro Guessing Program!");
 ti.output("This program has a list of the 10 most well-known Linux distros.");
 let greaterThanF = await (ti.promptYesOrNo("Is your distro greater than or equal to the letter F?"));
@@ -20,13 +21,31 @@ if (greaterThanF===true){
       if (isUbuntu===true){
         ti.output ("yay i did it lets goooo");
       }else{
-        let isFedora = ("is your distro Fedora?")
+        let isFedora = await ti.promptYesOrNo("is your distro Fedora?")
         if (isFedora===true){
           ti.output("yay i did it");
         }
       }
     }
-  }
+  }else{
+    let greaterThanG = await ti.promptYesOrNo("Is your distro greater than or equal to the letter G?");
+    if (greaterThanG===true){
+      let isKDE = await ti.promptYesOrNo("Is your distro KDE Neon?");
+      if (isKDE===true){
+        ti.output("yay lets go i did it");
+      }else{
+        let isGentoo = await ti.promptYesOrNo("Is your distro Gentoo?");
+        if (isGentoo===true){
+          ti.output("yay i did it woooooooo")
+        }
+      }
+      }else{
+        let isFedora = await ti.promptYesOrNo("Is your distro Fedora?");
+        }if (isFedora===true){
+          ti.output("yayyy");
+      }
+    }
+  
 } else {
   greaterThanD = await ti.promptYesOrNo("Is your distro greater than or equal to the letter D?");
 }if (greaterThanD===true){
@@ -38,6 +57,9 @@ if (greaterThanF===true){
   }if (isDebian===true){
     ti.output ("yay I did it lets gooooooo");
   }else{
-    ti.output("no.")
+    no = true
+    alert("no.")
   }
 }
+if (no===false) ti.output("Thank you for playing! See you soon!!")
+if (no===true) ti.output ("get out of here.")
